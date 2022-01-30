e13145814658a9d8b756723e8026f9763372101a
JulioMontas <hello@juliomontas.com>
2022-01-30 00:33:16 -0500

    #cleaning, about page added

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+function AboutPage(props) {
+  return (
+    <Layout>
+      <Seo title="The Firm" />
+      <h1>The Firm</h1>
+      <p>Text</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
