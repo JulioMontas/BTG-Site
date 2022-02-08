1bb1a5113a1a034fc4af6ca63a3dee7bc456da77
JulioMontas <hello@juliomontas.com>
2022-02-08 15:24:05 -0500

    code cleaning

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,3 @@
 // gatsby-browser.js
-import "@fontsource/oswald/200.css"; // Defaults to weight 100.
 import "@fontsource/oswald/400.css"; // Defaults to weight 400.
 import "@fontsource/oswald/700.css"; // Defaults to weight 700.
--- gatsby-config.js
+++ gatsby-config.js
@@ -40,8 +40,6 @@ module.exports = {
         lang: `en`,
         start_url: `/`,
         background_color: `#3D586B`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
         theme_color: `#3D586B`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
