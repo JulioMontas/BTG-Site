23fbd23a17bc35b100c00cdbd0c7a3db55d084cc
JulioMontas <hello@juliomontas.com>
2022-03-09 14:15:53 -0500

    desktop view

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,25 @@
 import React, { useState } from "react"
+import PropTypes from "prop-types"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import styled, { createGlobalStyle } from "styled-components"
 
-const Wrapper = styled.div`
+const WaveBottom = styled.div`
+  position: absolute;
+  top: 1;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+  transform: rotate(180deg);
+  svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: 25px;
+    fill: var(--primary-bg-color);
+  }
+`
+const WrapperMobile = styled.div`
   position: fixed;
   width:100%;
   top: 0;
@@ -15,7 +32,6 @@ const Wrapper = styled.div`
     justify-content: space-between;
   }
 `
-
 const MenuIcon = styled.button`
   top: 2rem;
   right: 2rem;
@@ -27,7 +43,7 @@ const MenuIcon = styled.button`
   background: transparent;
   border: none;
   cursor: pointer;
-  z-index: 110;
+  z-index: 120;
   span {
     width: 1.5rem;
     height 0.20rem;
@@ -46,8 +62,11 @@ const MenuIcon = styled.button`
       transform: ${({nav}) => nav ? 'rotate(-45deg)' : 'rotate(0)'}
     }
   }
+  @media (min-width: 768px) {
+    display: none;
+  }
 `
-const MenuLinks = styled.nav`
+const MenuLinksMobile = styled.nav`
   display: flex;
   flex-direction: column:
   justify-content: center;
@@ -88,35 +107,54 @@ const MenuLinks = styled.nav`
   }
   @media (min-width: 768px) {
     width: 100%;
-    border: 10px solid #BCA360;
+    border: 5px solid #BCA360;
+    display: none;
   }
 `
-const WaveBottom = styled.div`
-  position: absolute;
-  top: 1;
-  left: 0;
-  width: 100%;
-  overflow: hidden;
-  line-height: 0;
-  transform: rotate(180deg);
-  svg {
-    position: relative;
-    display: block;
-    width: calc(100% + 1.3px);
-    height: 25px;
-    fill: var(--primary-bg-color);
+const MenuLinksDesktop = styled.nav`
+  display:none;
+  ul {
+    display:flex;
+    li {
+      margin-right: 20px;
+      :last-child{
+        margin-right: 0;
+      }
+    }
+    a {
+      text-transform: uppercase;
+      font-size: 1.1em;
+      letter-spacing: 1px;
+      color:rgba(255, 255, 255, 0.8);
+      :hover {
+        color: rgba(255, 255, 255, 1);
+      }
+    }
+  }
+  @media (min-width: 768px) {
+    display:block;
+  }
+`
+const SVGMobile = styled.svg`
+  @media (min-width: 768px) {
+    display:none;
+  }
+`
+const SVGDesktop = styled.svg`
+  display:none;
+  @media (min-width: 768px) {
+    display:block;
   }
 `
 
-const Navbar = () => {
+const Navbar = ({ siteTitle }) => {
   const [nav, showNav] = useState(false)
   return(
     <>
-      <Wrapper>
-
+      <WrapperMobile>
         <div className="container">
-        <AniLink cover to="/" bg="#3d586b">
-        {/* <svg width="240" height="43" viewBox="0 0 240 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <AniLink cover to="/" bg="#3d586b">
+            <SVGDesktop width="240" height="43" viewBox="0 0 240 43" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M114.149 22.7921H114.653C114.995 24.8108 116.437 26.9739 118.689 26.9739C120.041 26.9739 121.374 26.091 121.374 24.6313C121.374 20.5762 114.167 22.9732 114.167 18.1797C114.167 16.0171 116.095 14.7558 118.023 14.7558C119.663 14.7558 120.528 15.8004 120.96 15.8004C121.23 15.8004 121.464 15.1514 121.518 14.9714H122.131V19.1532H121.627C121.158 17.369 120.131 15.3692 118.041 15.3692C116.671 15.3692 115.644 16.1077 115.644 17.549C115.644 20.9907 122.852 19.1348 122.852 23.5678C122.852 26.0726 120.923 27.5862 118.528 27.5862C116.834 27.5862 116.186 26.6494 115.5 26.6494C115.032 26.6494 114.87 27.0272 114.653 27.4784H114.149V22.7921Z" fill="white"/>
     <path fill-rule="evenodd" clip-rule="evenodd" d="M62.3016 26.7577C60.7877 26.7577 60.4455 26.4516 60.4455 25.5142V21.1707H63.3099C65.0761 21.1707 65.8327 22.2881 65.8327 23.9817C65.8327 25.7659 65.0399 26.7577 62.3016 26.7577ZM60.4455 16.4855C60.4455 15.981 60.6077 15.5848 62.0855 15.5848C64.2833 15.5848 65.22 16.3055 65.22 18.1796C65.22 19.5854 64.6433 20.5584 62.2288 20.5584H60.4455V16.4855ZM63.7788 20.7934V20.7562C65.6883 20.7562 67.3099 19.621 67.3099 18.0357C67.3099 16.5577 66.1933 14.9713 62.7694 14.9713H56.95V15.5848C58.3011 15.5848 58.6077 15.9093 58.6077 16.7744L58.6072 27.3706H63.8144C66.8605 27.3706 67.9233 25.1347 67.9233 23.9817C67.9233 22.072 66.4455 20.8829 63.7788 20.7934Z" fill="white"/>
     <path fill-rule="evenodd" clip-rule="evenodd" d="M71.1833 22.4681L72.9855 17.7296L74.8955 22.4681H71.1833ZM77.7788 25.0463L73.8316 14.8997H73.1833L69.5438 24.848C69.1472 25.947 68.7688 26.7577 67.3088 26.7577V27.3706H69.4561L70.2672 25.0919C70.4472 24.5551 70.7333 23.8761 70.9316 23.226H75.1833C75.6699 24.2712 76.1922 25.712 76.1922 26.0909C76.1922 26.6127 76.1205 26.7577 74.7683 26.7577V27.3706H79.7783V26.7577C78.7327 26.7577 78.3188 26.4327 77.7788 25.0463Z" fill="white"/>
@@ -138,67 +176,107 @@ const Navbar = () => {
     <path fill-rule="evenodd" clip-rule="evenodd" d="M19.5973 10.1278V14.6263H23.6046V15.607H19.5973V26.0443C19.5973 27.255 20.3473 27.5429 21.1834 27.5429C22.3079 27.5429 23.2879 26.7366 23.2879 24.4301V23.8239H24.0946V24.314C24.0946 26.91 23.3168 28.5241 20.4623 28.5241C18.4446 28.5241 16.8879 27.8601 16.8879 25.0069V15.607H14.7834V14.6263C17.4646 14.3951 17.8107 13.645 18.5596 10.1278H19.5973Z" fill="white"/>
     <path fill-rule="evenodd" clip-rule="evenodd" d="M28.5488 18.7492C28.5488 21.114 29.5871 22.2381 31.1438 22.2381C32.6999 22.2381 33.7382 21.114 33.7382 18.7492C33.7382 16.3843 32.6999 15.2603 31.1438 15.2603C29.5871 15.2603 28.5488 16.3843 28.5488 18.7492ZM30.9705 33.0226C34.1416 33.0226 35.9293 31.6096 35.9293 30.0521C35.9293 28.6391 34.2571 27.7168 30.6538 27.7168C27.9727 27.7168 26.8188 28.8703 26.8188 29.9938C26.8188 31.6096 28.7221 33.0226 30.9705 33.0226ZM28.6066 27.1683C27.3666 27.1405 25.781 26.7077 25.781 25.178C25.781 23.9106 28.001 23.0454 29.0393 22.8426V22.7859C27.1938 22.152 25.6082 20.7673 25.6082 18.663C25.6082 16.5283 27.6843 14.2801 30.8843 14.2801C32.5277 14.2801 33.3343 14.5107 34.0843 14.9141C34.0843 13.53 34.8916 12.7226 36.2749 12.7226C37.1399 12.7226 38.0338 13.011 38.0338 14.3379C38.0338 14.9713 37.5438 15.4909 36.8805 15.4909C36.2171 15.4909 35.7277 15.1152 35.7277 14.539C35.7277 14.2507 35.7849 13.9912 35.8138 13.8473C35.7277 13.8184 35.641 13.8184 35.5832 13.8184C35.2949 13.8184 34.8332 14.194 34.8332 14.9141V15.2892C35.4393 15.7215 35.871 16.2121 36.1888 16.7311C36.506 17.2495 36.6788 17.8551 36.6788 18.6053C36.6788 20.4506 35.0349 22.8426 32.1816 23.0454C28.8949 23.276 27.9143 23.5927 27.9143 24.3718C27.9143 26.3321 38.0043 23.7944 38.0043 29.1003C38.0043 32.5325 33.9688 34.0017 30.9705 34.0017C27.511 34.0017 24.7427 32.3881 24.7427 30.3127C24.7427 28.5536 26.9343 27.3422 28.6066 27.2272V27.1683Z" fill="white"/>
     <path fill-rule="evenodd" clip-rule="evenodd" d="M41.4999 27.1017C41.4999 27.9846 40.8633 28.5224 40.0794 28.5224C39.2949 28.5224 38.6577 27.9846 38.6577 27.1017C38.6577 26.2187 39.2949 25.6814 40.0794 25.6814C40.8633 25.6814 41.4999 26.2187 41.4999 27.1017Z" fill="white"/>
-        </svg>
-        */}
-        <svg width="42" height="27" viewBox="0 0 42 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+          </SVGDesktop>
+            <SVGMobile width="42" height="27" viewBox="0 0 42 27" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M8.245 7.2603C6.05389 7.2603 4.72778 9.85517 4.72778 13.4885V14.8149C4.72778 18.1021 5.67944 19.5429 8.01444 19.5429C10.7244 19.5429 11.7622 16.8624 11.7622 13.4885C11.7622 9.85517 10.7244 7.2603 8.245 7.2603ZM7.89889 20.5241C5.535 20.5241 4.35278 18.9667 3.66111 18.9667C3.05611 18.9667 2.825 19.8312 2.62333 20.2358H1.64333C1.845 19.3128 2.01778 18.2171 2.01778 17.1214V2.70455C2.01778 1.78162 1.49889 1.49379 0 1.49379V0.513632L4.72778 0.340271V8.70164H4.78555C5.65 7.14528 6.86111 6.28013 8.47555 6.28013C12.1367 6.28013 14.7028 9.33619 14.7028 13.2285C14.7028 17.238 11.4733 20.5241 7.89889 20.5241Z" fill="white"/>
         <path fill-rule="evenodd" clip-rule="evenodd" d="M19.5973 2.12778V6.62629H23.6046V7.60701H19.5973V18.0443C19.5973 19.255 20.3473 19.5429 21.1834 19.5429C22.3079 19.5429 23.2879 18.7366 23.2879 16.4301V15.8239H24.0946V16.314C24.0946 18.91 23.3168 20.5241 20.4623 20.5241C18.4446 20.5241 16.8879 19.8601 16.8879 17.0069V7.60701H14.7834V6.62629C17.4646 6.39515 17.8107 5.64502 18.5596 2.12778H19.5973Z" fill="white"/>
         <path fill-rule="evenodd" clip-rule="evenodd" d="M28.5488 10.7492C28.5488 13.114 29.5871 14.2381 31.1438 14.2381C32.6999 14.2381 33.7382 13.114 33.7382 10.7492C33.7382 8.38434 32.6999 7.26027 31.1438 7.26027C29.5871 7.26027 28.5488 8.38434 28.5488 10.7492ZM30.9705 25.0226C34.1416 25.0226 35.9293 23.6096 35.9293 22.0521C35.9293 20.6391 34.2571 19.7168 30.6538 19.7168C27.9727 19.7168 26.8188 20.8703 26.8188 21.9938C26.8188 23.6096 28.7221 25.0226 30.9705 25.0226ZM28.6066 19.1683C27.3666 19.1405 25.781 18.7077 25.781 17.178C25.781 15.9106 28.001 15.0454 29.0393 14.8426V14.7859C27.1938 14.152 25.6082 12.7673 25.6082 10.663C25.6082 8.52825 27.6843 6.2801 30.8843 6.2801C32.5277 6.2801 33.3343 6.5107 34.0843 6.9141C34.0843 5.52998 34.8916 4.72263 36.2749 4.72263C37.1399 4.72263 38.0338 5.01101 38.0338 6.33789C38.0338 6.97133 37.5438 7.49086 36.8805 7.49086C36.2171 7.49086 35.7277 7.11524 35.7277 6.53904C35.7277 6.25066 35.7849 5.99117 35.8138 5.84725C35.7277 5.81836 35.641 5.81836 35.5832 5.81836C35.2949 5.81836 34.8332 6.19398 34.8332 6.9141V7.28916C35.4393 7.72145 35.871 8.21209 36.1888 8.73106C36.506 9.24948 36.6788 9.85514 36.6788 10.6053C36.6788 12.4506 35.0349 14.8426 32.1816 15.0454C28.8949 15.276 27.9143 15.5927 27.9143 16.3718C27.9143 18.3321 38.0043 15.7944 38.0043 21.1003C38.0043 24.5325 33.9688 26.0017 30.9705 26.0017C27.511 26.0017 24.7427 24.3881 24.7427 22.3127C24.7427 20.5536 26.9343 19.3422 28.6066 19.2272V19.1683Z" fill="white"/>
         <path fill-rule="evenodd" clip-rule="evenodd" d="M41.4999 19.1017C41.4999 19.9846 40.8633 20.5224 40.0794 20.5224C39.2949 20.5224 38.6577 19.9846 38.6577 19.1017C38.6577 18.2187 39.2949 17.6814 40.0794 17.6814C40.8633 17.6814 41.4999 18.2187 41.4999 19.1017Z" fill="white"/>
-        </svg>
-        </AniLink>
+            </SVGMobile>
+          </AniLink>
 
-        <MenuIcon nav={nav} onClick={() => showNav(!nav)}>
-          <span />
-          <span />
-          <span />
-        </MenuIcon>
+          <MenuLinksDesktop>
+            <ul>
+              <li>
+                <AniLink cover direction="left" to="/about-us" bg="#3d586b">
+                  About Us
+                </AniLink>
+              </li>
+              <li>
+                <AniLink cover direction="left" to="/services" bg="#3d586b">
+                  Services
+                </AniLink>
+              </li>
+              <li>
+                <AniLink cover direction="left" to="/testimonial-and-case-study" bg="#3d586b">
+                  Testimonial & Case Study
+                </AniLink>
+              </li>
+              <li>
+                <AniLink cover direction="left" to="/guide-and-resources" bg="#3d586b">
+                  Guide & Resources
+                </AniLink>
+              </li>
+              <li>
+                <AniLink cover direction="left" to="/contact" bg="#3d586b">
+                  Contact Us
+                </AniLink>
+              </li>
+              <li>
+                <a href="tel:5554280940">813-251-0777</a>
+              </li>
+            </ul>
+          </MenuLinksDesktop>
+
+          <MenuIcon nav={nav} onClick={() => showNav(!nav)}>
+            <span />
+            <span />
+            <span />
+          </MenuIcon>
         </div>
+
+        <MenuLinksMobile nav={nav}>
+          <ul>
+            <li>
+              <AniLink cover direction="left" to="/about-us" bg="#3d586b">
+                About Us
+              </AniLink>
+            </li>
+            <li>
+              <AniLink cover direction="left" to="/services" bg="#3d586b">
+                Services
+              </AniLink>
+            </li>
+            <li>
+              <AniLink cover direction="left" to="/testimonial-and-case-study" bg="#3d586b">
+                Testimonial & Case Study
+              </AniLink>
+            </li>
+            <li>
+              <AniLink cover direction="left" to="/guide-and-resources" bg="#3d586b">
+                Guide & Resources
+              </AniLink>
+            </li>
+            <li>
+              <AniLink cover direction="left" to="/contact" bg="#3d586b">
+                Contact Us
+              </AniLink>
+            </li>
+            <li>
+              <a href="tel:5554280940">813-251-0777</a>
+            </li>
+            <li>
+              <h4>Tampa, Florida Attorneys (FL)</h4>
+            </li>
+          </ul>
+        </MenuLinksMobile>
 
         <WaveBottom>
           <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
             <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
           </svg>
         </WaveBottom>
-      </Wrapper>
-
-      <MenuLinks nav={nav}>
-        <ul>
-          <li>
-            <AniLink cover direction="left" to="/about-us" bg="#3d586b">
-              About Us
-            </AniLink>
-          </li>
-          <li>
-            <AniLink cover direction="left" to="/services" bg="#3d586b">
-              Services
-            </AniLink>
-          </li>
-          <li>
-            <AniLink cover direction="left" to="/testimonial-and-case-study" bg="#3d586b">
-              Testimonial & Case Study
-            </AniLink>
-          </li>
-          <li>
-            <AniLink cover direction="left" to="/guide-and-resources" bg="#3d586b">
-              Guide & Resources
-            </AniLink>
-          </li>
-          <li>
-            <AniLink cover direction="left" to="/contact" bg="#3d586b">
-              Contact Us
-            </AniLink>
-          </li>
-          <li>
-            <a href="tel:5554280940">813-251-0777</a>
-          </li>
-          <li>
-            <h4>Tampa, Florida Attorneys (FL)</h4>
-          </li>
-        </ul>
-      </MenuLinks>
+      </WrapperMobile>
     </>
   )
+}
+
+Navbar.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Navbar.defaultProps = {
+  siteTitle: ``,
 }
 
 export default Navbar;
